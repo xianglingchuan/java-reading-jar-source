35759102a6c24f3d178b9d486483028c7786ee7a
xianglingchuan <xianglingchuan@sina.cn>
2017-11-06 16:22:30 +0800

    项目初始化提交

---
+++ src/main/java/com/learn/web/servlet/HomeServlet.java
@@ -0,0 +1,33 @@
+package com.learn.web.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 自动回复功能控制层 
+ */
+@SuppressWarnings("serial")
+public class HomeServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=utf-8");
+		PrintWriter out = resp.getWriter();		
+		String resultStr = "Hello world";
+		out.write(resultStr);
+		out.close();
+	}
+
+	
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		this.doGet(req, resp);
+	}	
+	
+}
+
